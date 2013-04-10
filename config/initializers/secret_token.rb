f3f611920d981fff3a7936fc68918a106e9355c8
Anatolii <anatoliy.ruchka@gmail.com>
2013-04-10 10:06:36 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BulletinApp::Application.config.secret_token = '72ed0a42434921b5f29d604128583534fea23af6182990fe2176e22acba4e10a0eccdfbf269e62454388cef0773e2d78678bcf18940e786b5e3ae53d8d978a98'
