351fa14d54ecb96186d272790fbf9e7ebf0d6707
Anatolii <anatoliy.ruchka@gmail.com>
2013-04-11 17:16:06 +0300

    Fix Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
-
+def full_title(page_title)
+    base_title= "BulletinApp Test Task"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
