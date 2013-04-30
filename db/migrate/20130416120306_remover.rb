dc4f366328e9dcccba4b97aa5e38bf14cff83dc4
Anatolii <anatoliy.ruchka@gmail.com>
2013-04-30 21:30:32 +0300

    Get through tests

--- db/migrate/20130416120306_remover.rb
+++
@@ -1,9 +0,0 @@
-class Remover < ActiveRecord::Migration
-  def up
-
-    remove_column :users, :gmaps, :boolean #not mandatory, see wiki
-  end
-
-  def down
-  end
-end
--- db/migrate/20130416266522_add_birthday_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddBirthdayToUsers < ActiveRecord::Migration
-change_table :users do |t|
-  t.remove :birthday
-  t.date :birthday, :default => Time.now
-
-end
-end
