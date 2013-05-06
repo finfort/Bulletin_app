eafacaa166765022d978662d9533d265147e9a56
Anatolii <anatoliy.ruchka@gmail.com>
2013-05-06 13:56:54 +0300

    Go through tests

--- app/helpers/omniauthcallbacks_helper.rb
+++
@@ -1,2 +0,0 @@
-module OmniauthcallbacksHelper
-end
--- db/migrate/20130415183754_add_details_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :city, :string
-    add_column :users, :state, :string
-    add_column :users, :country, :string
-    add_column :users, :zip, :decimal
-  end
-end
